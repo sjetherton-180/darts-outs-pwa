00f7dc0974b7524141101f5cf255be78647a8549
sjetherton-180 <sj.etherton@gmail.com>
2025-11-17 23:01:00 +0000

    Create Service-worker.js

---
+++ Service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'darts-outs-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './Manifest/manifest.json',
+  './Icons/192.png',
+  './Icons/512.png',
+  './style.css',    // if you have CSS
+  './script.js'     // if you have JS
+];
+
+// Install service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch cached files or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
