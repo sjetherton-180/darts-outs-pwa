f0de883304ac8c9caa993f91bc5d1e5831b24089
sjetherton-180 <sj.etherton@gmail.com>
2025-11-18 17:07:24 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,41 @@
-const CACHE_NAME = 'darts-outs-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest/manifest.json',
-  './Icons/192.png',
-  './Icons/512.png',
-  './style.css',    // if you have CSS
-  './script.js'     // if you have JS
+const CACHE_NAME = "darts-outs-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-192.png",
+  "./icons/icon-maskable-512.png"
 ];
 
-// Install service worker and cache files
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-// Activate service worker
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
-            return caches.delete(cache);
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
           }
         })
-      );
-    })
+      )
+    )
   );
+  self.clients.claim();
 });
 
-// Fetch cached files or network
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
