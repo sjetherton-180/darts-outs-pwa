9bcb4e0be190c4cb80a2855f638cdf8a66d72e81
sjetherton-180 <sj.etherton@gmail.com>
2025-11-19 20:27:24 +0000

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,1 +1,15 @@
+// Simple checkout list
+const checkoutRoutes = {
+    170: ["T20", "T20", "Bull"],
+    167: ["T20", "T19", "Bull"],
+    164: ["T20", "T18", "Bull"],
+    161: ["T20", "T17", "Bull"]
+};
 
+export function getRandomCheckout() {
+    const keys = Object.keys(checkoutRoutes);
+    const pick = keys[Math.floor(Math.random() * keys.length)];
+    return { score: pick, route: checkoutRoutes[pick] };
+}
+
+window.getRandomCheckout = getRandomCheckout;
