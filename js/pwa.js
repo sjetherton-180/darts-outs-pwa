9cffb7c01c2eb5306851209a448451603fceba8c
sjetherton-180 <sj.etherton@gmail.com>
2025-11-19 20:26:19 +0000

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -1,1 +1,6 @@
-
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("service-worker.js");
+    });
+}
