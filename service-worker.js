603d8ba7471eaf7f309b204c7e708353f7bfe418
sjetherton-180 <sj.etherton@gmail.com>
2025-11-22 10:30:00 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,35 @@
-const CACHE='cm170-v1';const FILES=['/','/index.html','/css/styles.css','/js/app.js','/js/dartboard.js','/js/checkout.js','/js/pwa.js','/manifest.json'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))) });
+const CACHE_NAME = "cm170-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/css/dartboard.css",
+  "/js/app.js",
+  "/js/dartboard.js",
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png",
+  "/assets/icons/maskable-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
